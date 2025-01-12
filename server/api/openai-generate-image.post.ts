b97bcdff2d03ed9c920a30f42f01d17085d87e10
Erin Dunn Halligan <erin@designcandy.com>
2025-01-12 10:30:47 -0600

    Update openai-generate-image.post.ts

--- server/api/openai-generate-image.post.ts
+++ server/api/openai-generate-image.post.ts
@@ -7,19 +7,14 @@ interface CreateImage {
   responseFormat?: string
   user?: string
 }
-
 type CreateImageRequestSize = '256x256' | '512x512' | '1024x1024'
-
 export default defineEventHandler(async (event) => {
   const body: CreateImage = await readBody(event)
   const { apiKey, prompt, size } = body
-
   const configuration = new Configuration({
     apiKey: useRuntimeConfig().openaiApiKey || apiKey
   })
-
   const openai = new OpenAIApi(configuration)
-
   let imageSize: CreateImageRequestSize = '1024x1024'
   switch (size) {
     case 'small':
@@ -32,17 +27,16 @@ export default defineEventHandler(async (event) => {
       imageSize = '1024x1024'
       break
   }
-
   try {
     const response = await openai.createImage({
       prompt,
       n: 1,
       size: imageSize
     })
-
     const imageUrl = response.data?.data[0]?.url ?? ''
     event.node.res.statusCode = 200
     return imageUrl
+    alert('Test0: '+imageUrl)
   } catch (error) {
     if ((error as any).response) {
       return sendError(event, createError({
